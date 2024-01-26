ec76df0a61c8481e6e6d46760d16aa4e27964682
Mazionach <mazafesio25@gmail.com>
2024-01-26 12:56:57 -0800

    Fixed 0 case

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last = number % 10 if number > 0 else 10 - number % 10
+    last = number % 10 if number >= 0 else 10 - number % 10
     print("{}".format(last), end="")
     return last
