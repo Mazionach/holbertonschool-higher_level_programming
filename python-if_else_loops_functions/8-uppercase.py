47cfc8b7d7e267d94a207cd29024fca9785c5039
Mazionach <mazafesio25@gmail.com>
2024-01-26 12:37:13 -0800

    Added uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    for i in str:
+        print(chr(upChar(i)), end="")
+    print("")
+
+
+def upChar(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return ord(c) - 32
+    else:
+        return ord(c)
