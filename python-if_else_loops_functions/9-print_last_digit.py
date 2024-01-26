f20b200ff41d14ed9d451227b352636b402ade92
Mazionach <mazafesio25@gmail.com>
2024-01-26 12:56:07 -0800

    Removed debug test

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,3 @@ def print_last_digit(number):
     last = number % 10 if number > 0 else 10 - number % 10
     print("{}".format(last), end="")
     return last
-
-print_last_digit(-1)
-print_last_digit(-803)
