c5ea8ad2b29d0a0ebe620c89560c1ef3b9e922f2
Mazionach <mazafesio25@gmail.com>
2024-01-26 16:47:55 -0800

    Fixed format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,12 @@
 from sys import argv
 
 if __name__ == "__main__":
-    print("{} arguments".format(len(argv) - 1))
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} argumentsi:".format(len(argv) - 1))
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
