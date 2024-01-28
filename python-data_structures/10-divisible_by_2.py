c085ffa91a92e6c6f042bdae29022a6ce8a54c08
Mazionach <mazafesio25@gmail.com>
2024-01-28 06:13:25 -0800

    Fixed format

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,4 +4,7 @@ def divisible_by_2(my_list=[]):
     r = list()
 
     for i in my_list:
-        r.append(i % 2 == 0)
+        if i % 2 == 0:
+            r += True
+        else:
+            r += False
