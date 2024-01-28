310963889a9f49fbdf85f05b959f3e3a10baf490
Mazionach <mazafesio25@gmail.com>
2024-01-28 05:04:37 -0800

    Added print matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for r in matrix:
+        s = ""
+        for i in range(0, len(r)):
+            if i != len(r) - 1:
+                print("{} ".format(r[i]), end="")
+            else:
+                print("{}".format(r[i]))
