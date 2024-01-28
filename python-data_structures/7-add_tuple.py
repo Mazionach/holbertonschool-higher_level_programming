dc75f72f5188a1e9356958e4a8a452a89ccad609
Mazionach <mazafesio25@gmail.com>
2024-01-28 06:19:35 -0800

    Fixed format

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,19 +5,19 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a = tuple_a
     b = tuple_b
     if len(a) < 1 and len(b) < 1:
-        c += 0
+        c.append(0)
     elif len(a) > 1 and len(b) < 1:
-        c += a[0]
+        c.append(a[0])
     elif len(a) < 1 and len(b) > 1:
-        c += b[0]
+        c.append(b[0])
     else:
-        c += a[0] + b[0]
+        c.append(a[0] + b[0])
     if len(a) < 2 and len(b) < 2:
-        c += 0
+        c.append(0)
     elif len(a) > 2 and len(b) < 2:
-        c += a[1]
+        c.append(a[1])
     elif len(a) < 2 and len(b) > 2:
-        c += b[1]
+        c.append(b[1])
     else:
-        c += a[1] + b[1]
+        c.append(a[1] + b[1])
     return tuple(c)
