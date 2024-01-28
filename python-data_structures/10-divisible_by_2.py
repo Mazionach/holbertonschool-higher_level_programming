4de02ae925ffb9625071a8cce9c68c8deadd91a8
Mazionach <mazafesio25@gmail.com>
2024-01-28 06:15:41 -0800

    Hotfix

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -5,6 +5,6 @@ def divisible_by_2(my_list=[]):
 
     for i in my_list:
         if i % 2 == 0:
-            r += True
+            r.append(True)
         else:
-            r += False
+            r.append(False)
