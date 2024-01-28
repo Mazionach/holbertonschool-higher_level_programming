0ad50fbe2c88af8a157ca0463b59080c157ad386
Mazionach <mazafesio25@gmail.com>
2024-01-28 06:25:57 -0800

    Fixed off by one

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,17 +6,17 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = tuple_b
     if len(a) < 1 and len(b) < 1:
         c.append(0)
-    elif len(a) > 1 and len(b) < 1:
+    elif len(a) >= 1 and len(b) < 1:
         c.append(a[0])
-    elif len(a) < 1 and len(b) > 1:
+    elif len(a) < 1 and len(b) >= 1:
         c.append(b[0])
     else:
         c.append(a[0] + b[0])
     if len(a) < 2 and len(b) < 2:
         c.append(0)
-    elif len(a) > 2 and len(b) < 2:
+    elif len(a) >= 2 and len(b) < 2:
         c.append(a[1])
-    elif len(a) < 2 and len(b) > 2:
+    elif len(a) < 2 and len(b) >= 2:
         c.append(b[1])
     else:
         c.append(a[1] + b[1])
