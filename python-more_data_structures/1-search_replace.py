953c2ff6c000920b6d62c1f4ca243a16e8c919f2
Mazionach <mazafesio25@gmail.com>
2024-01-31 05:57:11 -0800

    Fixed style

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,6 @@
 
 def search_replace(my_list, search, replace):
 
-    new = [replace if x==search else x for x in my_list]
+    new = [replace if x == search else x for x in my_list]
 
     return new
-
