915edc62233f1c878e9f23f359134d54ee10b7a5
Mazionach <mazafesio25@gmail.com>
2024-01-31 06:44:19 -0800

    Fixed empty dict issue

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,8 +2,9 @@
 
 def best_score(a_dictionary):
     big = None
-
+    if a_dictionary is None:
+        return None
     for k in a_dictionary.keys():
-        if big is None or big < a_dictionary[k]:
-            big = a_dictionary[k]
+        if big is None or a_dictionary[big] < a_dictionary[k]:
+            big = k
     return big
