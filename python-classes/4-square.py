50b2934501e1287cee67a1d07830f7113e28b3f3
Mazionach <mazafesio25@gmail.com>
2024-02-03 14:14:24 -0800

    Hotfix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
         Args:
             size: the size of the square
         """
-        self.size(size)
+        self.size = size
 
     @property
     def size(self):
@@ -26,18 +26,15 @@ class Square:
         The length of the side of the square,
         must be a positive integer
         """
-        return self.size
-
-    @__size.setter
-    def size(self, size):
-        if size is not None:
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-
         return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """Calculate area of square
