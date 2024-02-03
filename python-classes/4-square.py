982856a61522c63808014f955858fd33ec7123b0
Mazionach <mazafesio25@gmail.com>
2024-02-03 13:29:00 -0800

    Added size setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Square module
+
+This module defines and handles a square shape
+
+"""
+
+
+class Square:
+    """ Square class
+    """
+
+    def __init__(self, size=0):
+        """Initialize a square
+
+        Args:
+            size: the size of the square
+        """
+        self.size(size)
+
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Calculate area of square
+        """
+        return self.__size ** 2
