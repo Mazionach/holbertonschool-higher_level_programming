29e1d0d18444939c4e7efc214865c5391097eab1
Mazionach <mazafesio25@gmail.com>
2024-02-03 13:15:45 -0800

    Added size to square

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Square module
+
+This module defines and handles a square shape
+
+"""
+
+
+class Square:
+    """ Square class
+    """
+
+    def __init__(self, size)
+        """Initialize a square
+
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
