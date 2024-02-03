0bda4acbd048ceabf16101d4df55a08de49595b8
Mazionach <mazafesio25@gmail.com>
2024-02-03 08:38:03 -0800

    Added safe div

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        v = a / b
+    except Exception:
+        v = None
+    finally:
+        print("Inside result: {}".format(v))
+        return v
