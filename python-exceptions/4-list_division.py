d590139551de81cc51e1d512fecb316b53da218f
Mazionach <mazafesio25@gmail.com>
2024-02-03 09:10:05 -0800

    Added safe list div

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    n = []
+    for i in range(list_length):
+        n.append(0)
+
+    for i in range(list_length):
+        d1, d2 = 0, 0
+        try:
+            d1, d2 = my_list_1[i], my_list_2[i]
+        except Exception:
+            print("out of range")
+
+        try:
+            v = d1 / d2
+        except TypeError:
+            print("wrong type")
+            v = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            v = 0
+        finally:
+            n[i] = v
+    return n
