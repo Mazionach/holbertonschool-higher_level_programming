052b5c65463b1aff20cd172037af9e2ecddd4e5a
Mazionach <mazafesio25@gmail.com>
2024-02-03 13:41:01 -0800

    Hotfix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,23 +19,23 @@ class Square:
         """
         self.size(size)
 
-    def size(self):
-        """Retrieves the size of the square
-        """
-        return self.__size
-
-    def size(self, size):
-        """Sets the size of the square
+    def size(self, size=None):
+        """Sets or gets the size of the square
 
         The size must be a positive int,
         else an error will be raised
-        """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
-        self.__size = size
+        Args:
+            size: the size of the square
+        """
+        if size is not None:
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+
+        return self.__size
 
     def area(self):
         """Calculate area of square
