77c61ab448740d0d1ce2adaa59fc00ed416d2aec
Mazionach <mazafesio25@gmail.com>
2024-02-08 17:59:04 -0800

    Fixed use of class attribute

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return ""
-        return ((str(Rectangle.print_symbol) * self.width + "\n")
+        return ((str(self.print_symbol) * self.width + "\n")
                 * self.height)[:-1]
 
     def __repr__(self):
