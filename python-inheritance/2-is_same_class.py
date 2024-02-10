fc3852679c7a8a64f8308e7e7358d9b3fe1fe1ef
Mazionach <mazafesio25@gmail.com>
2024-02-10 08:45:10 -0800

    Fixed style

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,5 +7,6 @@ tests for same class
 
 
 def is_same_class(obj, a_class):
-    """ Returns true if obj is a_class """
+    """ Returns true if obj is a_class
+    """
     return type(obj) == a_class
