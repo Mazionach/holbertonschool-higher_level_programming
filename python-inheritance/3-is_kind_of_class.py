5bb7a615fc6079cd3bdc308fa74839c6aa621d35
Mazionach <mazafesio25@gmail.com>
2024-02-10 09:06:31 -0800

    Added similar class check

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Similar class function
+
+tests for similar class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Returns true if obj is or inherits a_class
+    """
+    return isinstance(obj, a_class)
