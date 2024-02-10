8b690d0dd4800bdf273998125191f7d8b8f92245
Mazionach <mazafesio25@gmail.com>
2024-02-10 08:43:53 -0800

    Added class check

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Same class function
+
+tests for same class
+"""
+
+
+def is_same_class(obj, a_class):
+    """ Returns true if obj is a_class """
+    return type(obj) == a_class
