4b51353117617ba42c9173c3439d4b0fe5a83e32
Mazionach <mazafesio25@gmail.com>
2024-02-11 13:12:07 -0800

    Changed print to use size instead

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -30,4 +30,4 @@ class Square(Rectangle):
         return super().area()
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__size, self.__size)
