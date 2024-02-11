3ceec0e4b2f150b6ad367da26879d263a2b244dd
Mazionach <mazafesio25@gmail.com>
2024-02-11 13:01:46 -0800

    Added square area

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -16,6 +16,15 @@ def Square(Rectangle):
     """
 
     def __init__(self, size):
+        """ Initializes a square
+
+        Has only size
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """ Area of square
+        same as rectangle """
+        return super().area()
