9beade3683266d961461573777a5d1fa811b18eb
Mazionach <mazafesio25@gmail.com>
2024-02-11 12:53:44 -0800

    Added size attribute

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -17,5 +17,5 @@ def Square(Rectangle):
 
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__height = size
-        self.__width = size
+        self.__size = size
+        super().__init__(self.__size, self.__size)
