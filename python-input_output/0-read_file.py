d60fe72af2ea267a3d1bc18a4b1bdef2d4d6a357
Mazionach <mazafesio25@gmail.com>
2024-02-11 13:32:10 -0800

    Added documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,8 @@ Reading a simple file
 
 
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
-        print(f.read())
+    """ Reads file
+    prints file to stdout, no line ending
+    """
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
