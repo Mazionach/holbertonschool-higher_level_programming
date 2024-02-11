98fa0a1297cc3da9c909d49ffad7b5b0d3dcf452
Mazionach <mazafesio25@gmail.com>
2024-02-11 13:08:25 -0800

    Added square print

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Square module
+
+defines a square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class
+    Based on rectangle
+    """
+
+    def __init__(self, size):
+        """ Initializes a square
+
+        Has only size
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """ Area of square
+        same as rectangle """
+        return super().area()
