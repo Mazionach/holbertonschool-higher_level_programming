214a80e4dc841bb283772578b88622eec439d1f7
Mazionach <mazafesio25@gmail.com>
2024-02-11 08:34:28 -0800

    Added area placeholder

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Base geometry module
+
+For doing geometry
+"""
+
+
+class BaseGeometry():
+    """ Class for handling geometry
+    """
+
+    def area(self):
+        """ Not implemented yet """
+        raise Exception("area() is not implemented)
