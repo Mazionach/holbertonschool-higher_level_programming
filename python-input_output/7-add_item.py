1d26d054c8f97273dfd3e9834ec444934506f2fc
Mazionach <mazafesio25@gmail.com>
2024-02-11 17:16:33 -0800

    Added loading and appending of contents

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,6 +8,10 @@ Arguments to JSON file
 
 import sys
 save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
+x = load("add_item.json")
 
-save(sys.argv, "add_item.json")#l)
+x += sys.argv
+
+save(x, "add_item.json")
