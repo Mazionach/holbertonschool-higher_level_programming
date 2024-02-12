f273ad175304c1672fc994a11377b868e5841296
Mazionach <mazafesio25@gmail.com>
2024-02-11 16:30:52 -0800

    Added file appending

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Appending to a simple file
+"""
+
+
+def append_file(filename="", text=""):
+    """ Writes to file
+    Extends existing file
+    """
+    with open(filename, "a", encoding='utf-8') as f:
+        return f.write(str(text))
