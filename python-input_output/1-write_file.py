696e753f62321ea38656667b56e6f9f92468b9c5
Mazionach <mazafesio25@gmail.com>
2024-02-11 16:24:19 -0800

    Added file writing

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Writing to a simple file
+"""
+
+
+def write_file(filename="", text=""):
+    """ Writes to file
+    Creates if doesnt exists, overwrites otherwise
+    """
+    with open(filename, "w", encoding='utf-8') as f:
+        f.write(text)
