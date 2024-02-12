8cc52678c0cf2c51c37a10c5dcaa247525908e27
Mazionach <mazafesio25@gmail.com>
2024-02-11 17:22:52 -0800

    Fixed no file issue

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,11 +7,15 @@ Arguments to JSON file
 
 
 import sys
+
+
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-x = load("add_item.json")
+try:
+    x = load("add_item.json")
+except FileNotFoundError:
+    x = []
 
-x += sys.argv
-
+x += sys.argv[1:]
 save(x, "add_item.json")
