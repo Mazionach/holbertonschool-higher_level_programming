3deb7b0cef4ee994babbc730a43758e2747697c4
Mazionach <mazafesio25@gmail.com>
2024-02-17 04:45:11 -0800

    Added tests for Base class

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -3,14 +3,21 @@
 """
 Tests for model shapes
 """
-
+from models import base
 
 import unittest
 
 class TestBase(unittest.TestCase):
     
     def test___init__(self):
-        pass
+        self.A = base.Base()
+        self.assertEqual(self.A.id, 1)
+        self.B = base.Base()
+        self.assertEqual(self.B.id, 2)
+        self.C = base.Base(16)
+        self.assertEqual(self.C.id, 16)
+        self.D = base.Base(-10)
+        self.assertEqual(self.D.id, -10)
 
 
 if __name__ == "__main__":
