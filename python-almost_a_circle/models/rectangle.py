d4d95fc6988b8920ab1431c7f7910e484d73faf9
Mazionach <mazafesio25@gmail.com>
2024-02-17 05:05:28 -0800

    Added rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""
+Rectangle module
+"""
+
+
+from base import Base
+
+
+class Rectangle(Base):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Width of rectangle """
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Height of rectangle """
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ X position of rectangle """
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__x
+
+    @y.setter
+    def y(self, value):
+        """ Y position of rectangle """
+        self.__y = value
