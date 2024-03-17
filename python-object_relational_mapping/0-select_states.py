7a512a66f7b6045fdb9bbb2c59c219372f206b5e
Mazionach <mazafesio25@gmail.com>
2024-03-17 14:51:51 +0000

    Added get states script

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Module for handling city database
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
+
+    states = cursor.fetchall()
+
+    for r in states:
+        print(r)
+
+    cur.close()
+    db.close()
